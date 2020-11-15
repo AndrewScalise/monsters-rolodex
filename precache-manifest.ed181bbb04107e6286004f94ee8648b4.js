75cf3998d3d07f51b8f22c7ab119baf15aa1f454
Mugen <paradigmcomplex@gmail.com>
2020-11-15 16:46:15 -0600

    Updates

---
+++ precache-manifest.ed181bbb04107e6286004f94ee8648b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb9231c2b93b7999c3b68c547e3e5fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "955c8807cbb9ba139bc6",
+    "url": "/monsters-rolodex/static/css/main.30472d7f.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955c8807cbb9ba139bc6",
+    "url": "/monsters-rolodex/static/js/main.40c075d6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
